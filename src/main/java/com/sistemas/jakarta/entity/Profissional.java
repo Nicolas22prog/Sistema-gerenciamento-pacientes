65a4200bb5030ef590ae15e9c11689ad663e20dc
Nicolas Monteiro <desenv7@mercadoturbo.com.br>
2025-09-03 14:08:20 -0300

    correção bug onde as entidades estavam sem construtor vazio

--- src/main/java/com/sistemas/jakarta/entity/Profissional.java
+++ src/main/java/com/sistemas/jakarta/entity/Profissional.java
@@ -29,6 +29,11 @@ public class Profissional {
         this.dataCriacao = dataCriacao;
     }
 
+    public Profissional() {
+    }
+
+    
+    
     public Long getId() {
         return id;
     }
--- src/main/java/com/sistemas/jakarta/entity/Relacionamento.java
+++ src/main/java/com/sistemas/jakarta/entity/Relacionamento.java
@@ -20,4 +20,67 @@ public class Relacionamento {
     private OffsetDateTime dataFim;
     private String papel;
 
+    public Relacionamento(Paciente pacienteId, Profissional proficonalId, OffsetDateTime dateInicio, OffsetDateTime dataFim, String papel) {
+        
+        this.pacienteId = pacienteId;
+        this.proficonalId = proficonalId;
+        this.dateInicio = dateInicio;
+        this.dataFim = dataFim;
+        this.papel = papel;
+    }
+
+    public Relacionamento() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public Paciente getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(Paciente pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public Profissional getProficonalId() {
+        return proficonalId;
+    }
+
+    public void setProficonalId(Profissional proficonalId) {
+        this.proficonalId = proficonalId;
+    }
+
+    public OffsetDateTime getDateInicio() {
+        return dateInicio;
+    }
+
+    public void setDateInicio(OffsetDateTime dateInicio) {
+        this.dateInicio = dateInicio;
+    }
+
+    public OffsetDateTime getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(OffsetDateTime dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getPapel() {
+        return papel;
+    }
+
+    public void setPapel(String papel) {
+        this.papel = papel;
+    }
+
+    
+    
+    
 }
